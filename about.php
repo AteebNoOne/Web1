b8fc05798f73a32cf136117d26a8fda101926610
unknown <ateebnoone@gmail.com>
2023-02-03 21:29:00 +0500

    first

---
+++ about.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="container">
+
+<?php @include 'header.php'; ?>
+
+<section class="about">
+
+   <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe4XoU7ji3J4rRINBuDX6Q2wcj0czoTFgI3PXLrVDguN7DNyp5rgGZ2UDGCuCxxbfvkpk&usqp=CAU" alt="">
+   <h3>about us</h3>
+   <p>We would be happy to assist you on your wedding in Norway!  We are professional wedding planners, based near Oslo, Pakistan. We have more than 20 years experience, and we travel all over Pakistan for you. We know what papers you need, we know all the best vendors for your arrangement, and we plan, design and ccoordinate your big day. Every wedding and couple is unique, and because of that we organize bespoke weddings.</p>
+   <a href="contact.php" class="btn">contact us</a>
+
+</section>
+
+<section class="team">
+
+   <h1 class="heading">our team</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/team-1.jpg" alt="">
+         <h3>Dua Khan</h3>
+         <p>Planner</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/team-2.jpg" alt="">
+         <h3>Asad Ahmed</h3>
+         <p>Marketing Director</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/team-3.jpg" alt="">
+         <h3>Sana Khan</h3>
+         <p>Head Designer</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="images/team-4.jpg" alt="">
+         <h3>Saad </h3>
+         <p>Photography Head</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<?php @include 'footer.php'; ?>
+
+</div>
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ dashboard.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+ 
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Welcome to the Dashboard</h2>
+        <p>Hello, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. You are now logged in.</p>
+        <p><a href="logout.php">Logout</a></p>
+    </div>
+</body>
+</html>
